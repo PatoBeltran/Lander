52d7fd5dbdc26024324542e8e87eb046f79fd28a
Abraham Kuri <kurenn@icalialabs.com>
2014-11-15 16:34:38 -0600

    Adds confirmable devise module

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.default_url_options = { :host => ENV['HOST_DEFAULT_URL'],
+                                               only_path: false }
+
+  config.action_mailer.asset_host = "http://#{ENV['HOST_DEFAULT_URL']}"
 end
