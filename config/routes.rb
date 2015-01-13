79f6b7574f6ce824bd58d121b7ccac21030be5d6
PatoBeltran <pbelsal@gmail.com>
2015-01-12 18:54:13 -0600

    Config routes to make API not contained by subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,19 @@ Rails.application.routes.draw do
     get "/" => "teams#show"
     resources :channels, only: [:create, :update, :destroy]
     resources :invitations, only: [:create]
+  end
 
-    namespace :api do
-      api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.lander.io+json; version=1", default: true}) do
-        resources :links, only: [:create, :index]
-        resources :sessions, only: [:create]
+  namespace :api do
+    api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.lander.io+json; version=1", default: true}) do
+      resources :links, only: [:create, :index]
+      resources :sessions, only: [:create]
+      resources :channels, only: [:index]
 
-        delete 'sessions' => 'sessions#destroy'
-      end
+      delete 'sessions' => 'sessions#destroy'
     end
   end
 
   devise_for :users
-
   root to: "teams#new"
-
   resources :teams, only: [:new, :create]
 end
